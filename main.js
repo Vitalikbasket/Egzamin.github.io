8a76544185a6282cb46b139be031054a9e8d7123
Vitalikbasket <144212143+Vitalikbasket@users.noreply.github.com>
2024-04-24 10:23:28 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function lista do kontaktu() {
-	let imie = document.getElementById("imie");
-	let nazwisko = document.getElementById("nazwisko");
-	let email = document.getElementById("email").value.toLowerCase();
-	let lista = document.getElementById("lista");
-	let wynik = document.getElementById("wynik");
-	wynik.innerHTML = imie.value + " " + nazwisko.value + "<br/>" + email + "<br/>" + "Usługa: " + lista.value;
-}
